6316d85d161e0669a061447433fe61f6fc4a245e
RaveTr <81907436+RaveTr@users.noreply.github.com>
2021-09-07 21:29:36 +0200

    extra stuff

--- src/main/java/com/slepmel/capable/common/items/ZeedriumPickaxeItem.java
+++ src/main/java/com/slepmel/capable/common/items/ZeedriumPickaxeItem.java
@@ -16,6 +16,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -31,10 +32,19 @@ public class ZeedriumPickaxeItem extends PickaxeItem{
 	public boolean mineBlock(@Nonnull ItemStack stack, @Nonnull World world, @Nonnull BlockState state, @Nonnull BlockPos pos, @Nonnull LivingEntity entity) {
 		if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SILK_TOUCH, stack) < 1) {
 			Block block = state.getBlock();
-			if (block instanceof CapableOreBlock || block instanceof OreBlock || block instanceof RedstoneOreBlock) {
+			if (block instanceof CapableOreBlock || block instanceof OreBlock) {
 				ItemStack drop = new ItemStack(block);
 				double chance = Common.zeedriumChancesOfSpecialProperties.get();
 				if (world.random.nextDouble() < chance && Common.enableZeedriumPickaxeProperties.get()) {
+					world.addFreshEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), drop));
+				}
+			}
+			else if (block instanceof RedstoneOreBlock) {
+				ItemStack drop = new ItemStack(Items.REDSTONE);
+				double chance = Common.zeedriumChancesOfSpecialProperties.get();
+				if (world.random.nextDouble() < chance && Common.enableZeedriumPickaxeProperties.get()) {
+                    int i = world.random.nextInt(10);
+                    for (int x = 0; x <= i; x++)
 					world.addFreshEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), drop));
 				}
 			}
